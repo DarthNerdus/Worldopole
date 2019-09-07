3e2c34d434ec11a86eadc66f24aa84384663c070
Jesse Read <jesse@jesseread.net>
2019-09-07 13:23:01 -0400

    Drop gym average display

--- pages/gym.page.php
+++ pages/gym.page.php
@@ -27,13 +27,8 @@
 		<h2 style="margin:0;"><img src="core/img/<?= $lower_team_name; ?>.png" alt="<?= $locales->TEAM; ?> <?= $team_name; ?> logo" width=50 style="display:inline-block;" class="team-logo" /> <?= $locales->TEAM; ?> <?= $locales->$upper_team_name; ?></h2>
 
 		<div class="row" style="margin-top:1em;">
-			<div class="col-xs-6">
 				<p><big><strong class="gym-<?= $team_name; ?>-js"><span>0</span></strong></big><br><?= $locales->GYMS_OWNED; ?></p>
-			</div>
 
-			<div class="col-xs-6">
-				<p><big><strong><span class="average-<?= $team_name; ?>-js">0</span></strong></big><br><?= $locales->GYMS_AVERAGE; ?></p>
-			</div>
 		</div>
 
 		<div class="row">
